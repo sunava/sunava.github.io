e0bf9a077bcfcd430d8334e4111230f63f2c9e68
Vanessa Hassouna <33067562+sunava@users.noreply.github.com>
2023-02-03 22:47:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function returnText(){
-    let input = document.getElementById("userInput").value;
-    alert(input)
+let input = localStorage.getItem('input')
+
+function returnText() {
+  input = document.getElementById("userInput").value
+  localStorage.setItem('input', input)
+  alert(input)
 }
+
+console.log(input)
