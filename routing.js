d3b3f456cd6b9f0fb76f1a7acb647d73ef349ec0
Vanessa Hassouna <33067562+sunava@users.noreply.github.com>
2023-02-03 23:04:51 +0100

    123

--- routing.js
+++
@@ -1,5 +0,0 @@
-// URL routing of the application.
-routing = new Routing();
-routing.ignore('*.js', '*.css', '*.gif', '*.png', '*.jpg', '*.jsp', '*.html');
-routing.add('/api/comment', Packages.gig.examples.guestbook.GuestbookEndpoint, 'post');
-routing.add('/api/comments', Packages.gig.examples.guestbook.GuestbookEndpoint, 'list');
